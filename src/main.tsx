922f60905e5d1b99a7f055dfefc5a6b55b7ccd7a
lord <danipramoedya2002@gmail.com>
2025-09-07 11:45:39 +0700

    feat(signup): Implementation of the basic appearance of the signup page

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,19 @@ import Layout from './components/User/Layout.tsx'
 import UserSignIn from "./components/User/UserSignIn.tsx"
 
 import { BrowserRouter, Routes, Route } from "react-router"
+import UserSignUp from './components/User/UserSignUp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+
         <Route element={<Layout />}>
           <Route path="/auth/signin" element={<UserSignIn />} />
+          <Route path="/auth/signup" element={<UserSignUp />} />
         </Route>
+
       </Routes>
     </BrowserRouter>
   </StrictMode>,
