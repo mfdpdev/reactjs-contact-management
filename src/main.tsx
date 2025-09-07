d03dd706c7dc93681144a785b6f6f227dffa3343
lord <danipramoedya2002@gmail.com>
2025-09-07 11:39:46 +0700

    feat(signin): Implementation of the basic appearance of the signin page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import Layout from './components/User/Layout.tsx'
+import UserSignIn from "./components/User/UserSignIn.tsx"
+
 import { BrowserRouter, Routes, Route } from "react-router"
 
 createRoot(document.getElementById('root')!).render(
@@ -10,6 +13,9 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route element={<Layout />}>
+          <Route path="/auth/signin" element={<UserSignIn />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
