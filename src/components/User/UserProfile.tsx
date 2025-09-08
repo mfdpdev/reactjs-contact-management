086ef3601d718ab03e06b1f1d184cb509446684f
lord <danipramoedya2002@gmail.com>
2025-09-08 14:34:56 +0700

    feat(dashboard): Validation implementation replace password with formik and yup

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -1,17 +1,7 @@
-import { useState } from "react"
-
 import { Formik, Form, Field, ErrorMessage } from "formik"
 import * as Yup from "yup"
 
 export default function UserProfile(){
-
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
-
-  function handleSubmitPassword(){
-
-  }
-
   return (
     <>
       <div className="flex items-center mb-6">
@@ -107,50 +97,72 @@ export default function UserProfile(){
 
         <div
           className="bg-gray-800 bg-opacity-80 rounded-xl shadow-custom border border-gray-700 overflow-hidden card-hover animate-fade-in">
-          <form className="p-6 h-full flex flex-col justify-between" onSubmit={handleSubmitPassword}>
-            <div className="">
-              <div className="flex items-center mb-4">
-                <div className="w-10 h-10 bg-gray-700 rounded-lg flex items-center justify-center mr-3 shadow-md">
-                  <i className="fas fa-key text-white"></i>
-                </div>
-                <h2 className="text-xl font-semibold text-white">Change Password</h2>
-              </div>
-
-              <div className="mb-5">
-                <label htmlFor="new_password" className="block text-gray-300 text-sm font-medium mb-2">New
-                  Password</label>
-                <div className="relative">
-                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <i className="fas fa-lock text-gray-500"></i>
+          <Formik
+            initialValues={{password: '', confirmPassword: ""}}
+            validationSchema={
+              Yup.object({
+                password: Yup.string().min(8, "Password must be at least 8 characters").matches(/^(?=.*[A-Z])(?=.*\d).{6,}$/, 'Password must contain at least one uppercase letter and one number').required('Password is required'),
+                confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'Passwords must match').required('Confirm password is required'),
+              })
+            }
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {() => (
+              <Form className="p-6 h-full flex flex-col justify-between" >
+                <div className="">
+                  <div className="flex items-center mb-4">
+                    <div className="w-10 h-10 bg-gray-700 rounded-lg flex items-center justify-center mr-3 shadow-md">
+                      <i className="fas fa-key text-white"></i>
+                    </div>
+                    <h2 className="text-xl font-semibold text-white">Change Password</h2>
                   </div>
-                  <input type="password" id="new_password" name="new_password"
-                         className="w-full pl-10 pr-3 py-3 bg-gray-700 bg-opacity-50 border border-gray-600 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
-                         placeholder="Enter your new password" required
-                         value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-              </div>
 
-              <div className="mb-5">
-                <label htmlFor="confirm_password" className="block text-gray-300 text-sm font-medium mb-2">Confirm New
-                  Password</label>
-                <div className="relative">
-                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <i className="fas fa-check-double text-gray-500"></i>
+                  <div className="mb-5">
+                    <label htmlFor="new_password" className="block text-gray-300 text-sm font-medium mb-2">New
+                      Password</label>
+                    <div className="relative">
+                      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <i className="fas fa-lock text-gray-500"></i>
+                      </div>
+                      <Field type="password" id="password" name="password"
+                             className="w-full pl-10 pr-3 py-3 bg-gray-700 bg-opacity-50 border border-gray-600 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
+                             placeholder="Enter your new password" />
+                    </div>
+                    <ErrorMessage name="password">
+                      {(msg) => <div className="text-red-500 text-sm mt-2">{msg}</div>}
+                    </ErrorMessage>
                   </div>
-                  <input type="password" id="confirm_password" name="confirm_password"
-                         className="w-full pl-10 pr-3 py-3 bg-gray-700 bg-opacity-50 border border-gray-600 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
-                         placeholder="Confirm your new password" required
-                         value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+
+                  <div className="mb-5">
+                    <label htmlFor="confirmPassword" className="block text-gray-300 text-sm font-medium mb-2">Confirm New
+                      Password</label>
+                    <div className="relative">
+                      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <i className="fas fa-check-double text-gray-500"></i>
+                      </div>
+                      <Field type="password" id="confirmPassword" name="confirmPassword"
+                             className="w-full pl-10 pr-3 py-3 bg-gray-700 bg-opacity-50 border border-gray-600 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
+                             placeholder="Confirm your new password" />
+                    </div>
+                    <ErrorMessage name="confirmPassword">
+                      {(msg) => <div className="text-red-500 text-sm mt-2">{msg}</div>}
+                    </ErrorMessage>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="mt-6">
-              <button type="submit"
-                      className="w-full bg-gradient text-white py-3 px-4 rounded-lg hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-800 transition-all duration-200 font-medium shadow-lg transform hover:-translate-y-0.5 flex items-center justify-center">
-                <i className="fas fa-key mr-2"></i> Update Password
-              </button>
-            </div>
-          </form>
+                <div className="mt-6">
+                  <button type="submit"
+                          className="w-full bg-gradient text-white py-3 px-4 rounded-lg hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-800 transition-all duration-200 font-medium shadow-lg transform hover:-translate-y-0.5 flex items-center justify-center">
+                    <i className="fas fa-key mr-2"></i> Update Password
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </>
